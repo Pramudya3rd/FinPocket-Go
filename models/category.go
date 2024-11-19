1728e82bbea4c87bb56580ca5234353cabaf7d19
pramudya3rd <pramudyakumala@gmail.com>
2024-11-19 18:20:06 +0700

    modified:   models/category.go

--- models/category.go
+++ models/category.go
@@ -1,8 +1,14 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Category struct {
+	gorm.Model
+
 	ID        string    `json:"id" gorm:"primaryKey"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
